f7a14f8b43e8821261d6f49e8d4ca6dc084489c6
talaaz <tala.1997.az@gmail.com>
2022-02-18 23:52:55 +0100

    arabic translation done + images + linkedIn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
  
 import { HomeComponent } from './screens/home/home.component';
 import { CollaborationsComponent } from './screens/collaborations/collaborations.component';
-
+import { AboutComponent2 } from './components/about2/about2.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'collaborations', component: CollaborationsComponent },
-
+ 
 ];
 
 @NgModule({
