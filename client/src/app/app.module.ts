1cd8249b495a5cdecf1472c8b4d05828c7b7d5a2
Hadi Horani <hadihorani@hotmail.com>
2022-03-10 23:44:26 +0100

    Added notifier in case of sign up success/failure and input validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
- //App 
+//App 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 //Screens
@@ -10,30 +10,31 @@ import { CollaborationsComponent } from './screens/collaborations/collaborations
 import { WhyToJoinComponent } from './components/whyToJoin/whyToJoin.component';
 import { SignUpComponent } from './components/signUp/signUp.component';
 import { CourseComponent } from './components/course/course.component';
-import { AboutComponent2 } from './components/about2/about2.component'; 
+import { AboutComponent2 } from './components/about2/about2.component';
 import { TeamComponent2 } from './components/team2/team2.component';
 //Service
 import { HttpClientModule } from '@angular/common/http';
- import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {PuserService} from '../app/services/puser.service'
-import {CollaboratorService} from '../app/services/collaborator.service';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PuserService } from '../app/services/puser.service'
+import { CollaboratorService } from '../app/services/collaborator.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
 //Translatte 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
 
- import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NotifierModule } from 'angular-notifier';
 
 // Factory function required during AOT compilation
 export function httpTranslateLoaderFactory(http: HttpClient) {
@@ -45,18 +46,18 @@ export function httpTranslateLoaderFactory(http: HttpClient) {
     AppComponent,
     AboutComponent2,
     HomeComponent,
-  CollaborationsComponent,
+    CollaborationsComponent,
     WhyToJoinComponent,
     SignUpComponent,
     CourseComponent,
     TeamComponent2
-   ],
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-     HttpClientModule,
-     FormsModule,
+    HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatStepperModule,
@@ -69,8 +70,8 @@ export function httpTranslateLoaderFactory(http: HttpClient) {
     MatButtonToggleModule,
     MatTabsModule,
     MatInputModule,
- 
-     TranslateModule.forRoot({
+    NotifierModule,
+    TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: httpTranslateLoaderFactory,
@@ -78,8 +79,8 @@ export function httpTranslateLoaderFactory(http: HttpClient) {
       }
     })
   ],
-  providers: [PuserService,CollaboratorService],
+  providers: [PuserService, CollaboratorService],
   bootstrap: [AppComponent],
-  
+
 })
 export class AppModule { }
--- client/src/app/components/signUp/signUp.component.ts
+++ client/src/app/components/signUp/signUp.component.ts
@@ -1,38 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators, NgForm } from '@angular/forms'
-import { FormsModule } from '@angular/forms'; 
+import {TranslateService} from '@ngx-translate/core';
+import { NotifierService } from 'angular-notifier';
 import { PuserService } from '../../services/puser.service'
 
- 
+
 @Component({
   selector: 'app-signUp',
   templateUrl: './signUp.component.html',
-  styleUrls: [ './signUp.component.css' ]
+  styleUrls: ['./signUp.component.css']
 })
 export class SignUpComponent implements OnInit {
-  puser= {
-    name:''
-  }
-  constructor(private puserService: PuserService) { }
-
-  ngOnInit():  void {
+  puser = {
+    name: ''
   }
 
-  
+  disableButton: boolean = false;
+
+  private readonly notifier: NotifierService;
+
+  constructor(private puserService: PuserService, private notifierService: NotifierService, private translate: TranslateService) {
+    this.notifier = notifierService;
+  }
+
+  ngOnInit(): void {
+  }
+
+
   createPUser(): void {
     const data = {
       name: this.puser.name,
-     };
+    };
 
     this.puserService.addPUser(data)
-    .subscribe(
-      response => {
-        console.log(response);
-       },
-      error => {
-        console.log(error);
-      });
+      .subscribe(
+        response => {
+          this.notifier.notify('success', this.translate.instant('BODY.SignUp.NOTIFIER.SUCCESSMSG'));
+          this.disableButton = true;
+        },
+        error => {
+          this.notifier.notify('error', error);
+        });
   }
-  
-  
+
+
 }
